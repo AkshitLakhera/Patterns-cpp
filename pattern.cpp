04e31058acaff571f8936b450eaa656dc223e948
“[Akshit <akshitlakhera14@gmail.com>
2024-07-02 12:52:08 +0530

    Practice  6 and 7 patterns

--- pattern.cpp
+++ pattern.cpp
@@ -3,72 +3,72 @@
 #include <bits/stdc++.h>
 using namespace std;
 /* Pattern 1*/
-// void print(int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j < n; j++)
-//         {
-//             cout << "*";
-//         }
-//         cout << endl;
-//     }
-// }
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 
 /* Pattern2*/
-// void print(int n)
-// {
-//     for (int i = 0; i < n; i++)
-//     {
-//         for (int j = 0; j <= i; j++)
-//         {
-//             cout << "*";
-//         }
-//         cout << endl;
-//     }
-// }
+void print2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 
 /*  Pattern3 */
-// void print(int n)
-// {
-//     for (int i = 1; i <= n; i++)
-//     {
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout << " " << j;
-//         }
-//         cout << endl;
-//     }
-// }
+void print3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " " << j;
+        }
+        cout << endl;
+    }
+}
 
 /* Pattern4*/
-// void print(int n)
-// {
-//     for (int i = 1; i <= n; i++)
-//     {
-//         for (int j = 1; j <= i; j++)
-//         {
-//             cout << i << " ";
-//         }
-//         cout << endl;
-//     }
-// }
+void print4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
 
 /*Pattern5*/
 // my way
-// void print(int n)
-// {
-//     for (int i = 0; i <= n; i++)
-//     {
-//         for (int j = n; j >= i; j--)
-//         {
-//             cout << "*" << " ";
-//         }
-//         cout << endl;
-//     }
-// }
+void print5(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
 // striver way
-void print(int n)
+void printS(int n)
 {
     for (int i = 1; i <= n; i++)
     {
@@ -79,9 +79,44 @@ void print(int n)
         cout << endl;
     }
 }
+/*Pattern 6*/
+void print6(int n)
+{
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+/*pattern 7*/
+void print7(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "* ";
+        }
+        // space
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
 int main()
 {
     int n;
     cin >> n;
-    print(n);
+    print7(n);
 }
