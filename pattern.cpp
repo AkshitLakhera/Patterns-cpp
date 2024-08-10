e424586eb662274e375bd3c0ae3b41dd6f936840
“[Akshit <akshitlakhera14@gmail.com>
2024-08-10 23:31:21 +0530

    pattern updated

--- pattern.cpp
+++ pattern.cpp
@@ -172,7 +172,83 @@ void print11(int n)
         for (int j = 0; j < i; j++)
         {
             cout << start;
+            // flip the start
             start = 1 - start;
+        }
+        cout << endl;
+    }
+}
+void print12(int n)
+{
+    int space = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+        // number
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        // space
+        if (space > 0)
+        {
+            for (int j = 1; j <= space; j++)
+            {
+                cout << " ";
+            }
+        }
+        // number
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+        cout << endl;
+        space -= 2;
+    }
+}
+void print13(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num = num + 1;
+        }
+        cout << endl;
+    }
+}
+void print14(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        // here looping is done in the character
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+void print15(int n)
+{
+    for (int i = 0; i <= 0; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        };
+        cout << endl;
+    }
+}
+void print16(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        char ch = 'A' + i;
+        for (int j = 0; j < i; j++)
+        {
+            cout << "ch ";
         }
         cout << endl;
     }
@@ -181,5 +257,5 @@ int main()
 {
     int n;
     cin >> n;
-    print11(n);
+    print14(n);
 }
